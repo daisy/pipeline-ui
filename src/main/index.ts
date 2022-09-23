99b53180d7a3256a82fcf8fb9b669dd64083eadd
Nicolas Pavie <pavie.nicolas@gmail.com>
2022-09-23 16:03:05 +0200

    fix(ipc): send state to window on pipeline launch

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,15 +1,27 @@
 import { app } from 'electron'
 
-import { makeAppSetup, makeAppWithSingleInstanceLock, Pipeline2IPC, registerPipeline2ToIPC, } from './factories'
-import { MainWindow, registerAboutWindowCreationByIPC, PipelineTray } from './windows'
+import {
+  makeAppSetup,
+  makeAppWithSingleInstanceLock,
+  Pipeline2IPC,
+  registerPipeline2ToIPC,
+} from './factories'
+import {
+  MainWindow,
+  registerAboutWindowCreationByIPC,
+  PipelineTray,
+} from './windows'
 import { setupFileDialogEvents } from './fileDialogs'
+import { IPC } from 'shared/constants'
 
 makeAppWithSingleInstanceLock(async () => {
   await app.whenReady()
   const pipelineInstance = new Pipeline2IPC()
   const mainWindow = await makeAppSetup(MainWindow)
 
-  pipelineInstance.launch()
+  pipelineInstance.launch().then((state) => {
+    mainWindow.webContents.send(IPC.PIPELINE.STATE.CHANGED, state)
+  })
   const tray = new PipelineTray(mainWindow, null, pipelineInstance)
 
   registerAboutWindowCreationByIPC()

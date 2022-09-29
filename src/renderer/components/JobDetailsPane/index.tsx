0805b8b363d83073813d236e565f52c735b3e28b
Marisa DeMeglio <marisa.demeglio@gmail.com>
2022-09-29 16:24:04 -0700

    Experiment with job data fetching
    Sometimes the UI hangs if several jobs are going at once

--- src/renderer/components/JobDetailsPane/index.tsx
+++ src/renderer/components/JobDetailsPane/index.tsx
@@ -1,40 +1,43 @@
 import { useQuery } from '@tanstack/react-query'
 import { jobXmlToJson } from 'renderer/pipelineXmlConverter'
 import styles from './styles.module.sass'
+import { useEffect, useState } from 'react'
 
 export function JobDetailsPane( {job, removeJob, updateJob}) {
-  let jobData = null
   console.log('Job details pane', job.href)
   // get the rest of the job data
   const { isLoading, error, data } = useQuery([job.href], async () => {
     console.log('fetching job', job.href)
     let res = await fetch(job.href)
     let xmlStr = await res.text()
-    return xmlStr
-  }, { refetchInterval: 4000})
+    return jobXmlToJson(xmlStr)
+  }, {refetchInterval: 3000})
 
   if (isLoading) {
-    return <p>Loading...</p>
+    return <p>Loading job details...</p>
   }
   if (error instanceof Error) {
     return <p>Error {error.message}</p>
   }
+
+  
   if (!data) {
     console.log('no job data')
-    return <></>
+    
+    return (
+    <>
+    <p>Loading...</p>
+    </>)
   }
-  try {
-    jobData = jobXmlToJson(data)
-  }
-  catch (err) {
-    return <p>Error {err.message}</p>
-  }
+
+  
+
   return (
     <div className={styles.jobDetails}>
       <h2>Job</h2>
-      <p> {jobData.id}</p>
-      <p> {jobData.status}</p>
-      <code>{JSON.stringify(jobData)}</code>
+      <p> {data.id}</p>
+      <p> {data.status}</p>
+      <code>{JSON.stringify(data)}</code>
     </div>
   )
 }

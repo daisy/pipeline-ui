1be4ee6041ac098491807ead62181a10c46b770a
Marisa DeMeglio <marisa.demeglio@gmail.com>
2022-09-29 18:41:44 -0700

    Results path tweak
    and some light formatting

--- src/renderer/components/JobDetailsPane/index.tsx
+++ src/renderer/components/JobDetailsPane/index.tsx
@@ -4,16 +4,20 @@ import styles from './styles.module.sass'
 import { useEffect, useState } from 'react'
 const { App } = window
 
-export function JobDetailsPane( {job, removeJob, updateJob}) {
+export function JobDetailsPane({ job, removeJob, updateJob }) {
   console.log('Job details pane', job.href)
-  
+
   // get the rest of the job data
-  const { isLoading, error, data } = useQuery([job.href], async () => {
-    console.log('fetching job', job.href)
-    let res = await fetch(job.href)
-    let xmlStr = await res.text()
-    return jobXmlToJson(xmlStr)
-  }, {refetchInterval: 3000})
+  const { isLoading, error, data } = useQuery(
+    [job.href],
+    async () => {
+      console.log('fetching job', job.href)
+      let res = await fetch(job.href)
+      let xmlStr = await res.text()
+      return jobXmlToJson(xmlStr)
+    },
+    { refetchInterval: 3000 }
+  )
 
   if (isLoading) {
     return <p>Loading job details...</p>
@@ -22,32 +26,33 @@ export function JobDetailsPane( {job, removeJob, updateJob}) {
     return <p>Error {error.message}</p>
   }
 
-  
   if (!data) {
     console.log('no job data')
-    
+
     return (
-    <>
-    <p>Loading...</p>
-    </>)
+      <>
+        <p>Loading...</p>
+      </>
+    )
   }
-  
 
   return (
     <div className={styles.jobDetails}>
       <h2>Job: {data.nicename}</h2>
       <p> Status: {data.status}</p>
-      {data.status == 'SUCCESS' ? 
-        <JobResults jobId={data.jobId} results={data.results}/>
-      : ''}
+      {data.status == 'SUCCESS' ? (
+        <JobResults jobId={data.jobId} results={data.results} />
+      ) : (
+        ''
+      )}
     </div>
   )
 }
 
-function JobResults({jobId, results}) {
+function JobResults({ jobId, results }) {
   // this is a hack!
   // get the first file and use its path to figure out what is probably the output folder for the job
-  
+
   let file = ''
   if (results.namedResults.length > 0) {
     if (results.namedResults[0].files.length > 0) {
@@ -55,16 +60,18 @@ function JobResults({jobId, results}) {
       let idx = file.indexOf(jobId)
       if (idx != -1) {
         file = file.slice(0, idx + jobId.length) + '/'
+        file = file.replace('file:', '')
       }
     }
   }
 
-  
   if (file != '') {
-    return (<button onClick={e => App.copyToClipboard(file)}>Copy path to results</button>)
+    return (
+      <button onClick={(e) => App.copyToClipboard(file)}>
+        Copy path to results
+      </button>
+    )
+  } else {
+    return <p>Results unavailable</p>
   }
-  else {
-    return (<p>Results unavailable</p>)
-  }
-  
 }

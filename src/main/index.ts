7f68141651fd2541f88fe789bb86d6a652677df9
Nicolas Pavie <pavie.nicolas@gmail.com>
2022-10-28 16:18:02 +0200

    fix(menu): adding window custom menu

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,10 @@
-import { app, BrowserWindow } from 'electron'
+import {
+    app,
+    BrowserWindow,
+    Menu,
+    MenuItemConstructorOptions,
+    shell,
+} from 'electron'
 
 import { error } from 'electron-log'
 
@@ -46,4 +52,66 @@ makeAppWithSingleInstanceLock(async () => {
     bindWindowToPipeline(mainWindow, pipelineInstance)
     setupFileDialogEvents()
     setupShowInFolderEvents()
+    const isMac = process.platform === 'darwin'
+
+    // Template taken from electron documentation
+    // To be completed
+    const template: MenuItemConstructorOptions = [
+        // { role: 'appMenu' }
+        ...(isMac
+            ? [
+                  {
+                      label: app.name,
+                      submenu: [
+                          { label: 'Create a new job' },
+                          { type: 'separator' },
+                          { role: 'services' },
+                          { type: 'separator' },
+                          { role: 'hide' },
+                          { role: 'hideOthers' },
+                          { role: 'unhide' },
+                          { type: 'separator' },
+                          { role: 'quit' },
+                      ],
+                  },
+              ]
+            : []),
+        // { role: 'fileMenu' }
+        {
+            label: 'File',
+            submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+            ],
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn more',
+                    click: async () => {
+                        await shell.openExternal(
+                            'https://daisy.github.io/pipeline/'
+                        )
+                    },
+                },
+                {
+                    label: 'User guide',
+                    click: async () => {
+                        await shell.openExternal(
+                            'https://daisy.github.io/pipeline/Get-Help/'
+                        )
+                    },
+                },
+            ],
+        },
+    ]
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 })

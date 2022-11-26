7e3c01ebaa32bd6c46dd1efa1026baf87a1e1b2c
Marisa DeMeglio <marisa.demeglio@gmail.com>
2022-11-25 19:45:00 -0800

    feat: loading message
    Nothing else in the UI is available yet so we can just display this message

--- src/renderer/screens/Main/index.tsx
+++ src/renderer/screens/Main/index.tsx
@@ -14,7 +14,7 @@ export function MainScreen() {
                     {pipeline.status == PipelineStatus.RUNNING ? (
                         <MainView />
                     ) : (
-                        'WAITING'
+                        <p>Starting the DAISY Pipeline...</p>
                     )}
                 </main>
             </>

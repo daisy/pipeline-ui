78acdf617b9892c29a4ed8379dd183899ed22f15
Nicolas Pavie <pavie.nicolas@gmail.com>
2022-11-29 13:08:03 +0100

    fix(settings): update application settings window

--- src/renderer/screens/Settings/index.tsx
+++ src/renderer/screens/Settings/index.tsx
@@ -3,6 +3,9 @@ import { SettingsForm } from 'renderer/components/SettingsForm'
 export function SettingsScreen() {
     return (
         <>
+            <header>
+                <h1>Settings</h1>
+            </header>
             <main>
                 <h1>Settings</h1>
                 <SettingsForm />

7b00e4e5137373b80368e1d09c4648ed2c0491e5
Nicolas Pavie <pavie.nicolas@gmail.com>
2023-01-26 09:05:43 +0100

    fix(parser): exception for parsing debug
    Added a parser exception to also retrieve the text
    that failed to be parsed.

--- src/shared/parser/pipelineXmlConverter/parser.ts
+++ src/shared/parser/pipelineXmlConverter/parser.ts
@@ -1,14 +1,30 @@
 import { DOMParser } from '@xmldom/xmldom'
 
+export class ParserException extends Error {
+    parsedText: string
+    constructor(message?: string, options?: ErrorOptions, parsedText?: string) {
+        super(message, options)
+        this.parsedText = parsedText
+    }
+}
+
 // parse a string of xml and return the first element with the given name
 export function parseXml(xmlString, elmName) {
     let doc = new DOMParser().parseFromString(xmlString, 'text/xml')
     if (!doc) {
-        throw new Error(`Could not parse XML for ${elmName}`)
+        throw new ParserException(
+            `Could not parse the given string as XML (see parsedText field)`,
+            null,
+            xmlString
+        )
     }
     let elm = doc.getElementsByTagName(elmName)
     if (!elm || elm.length == 0) {
-        throw new Error(`Element ${elmName} not found`)
+        throw new ParserException(
+            `Could not parse XML for ${elmName} (see parsedText field)`,
+            null,
+            xmlString
+        )
     }
     return elm[0]
 }

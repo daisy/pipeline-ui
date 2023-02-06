f5b3700bc4675f573aae9936ed991d79d792bbe7
Nicolas Pavie <pavie.nicolas@gmail.com>
2023-02-06 09:21:41 +0100

    fix: job settings

--- src/renderer/components/JobDetailsPane/Settings.tsx
+++ src/renderer/components/JobDetailsPane/Settings.tsx
@@ -1,13 +1,19 @@
 // job settings, not application settings
+import { useWindowStore } from 'renderer/store'
 import { findValue, getAllOptional, getAllRequired } from 'renderer/utils/utils'
+import { Job } from 'shared/types'
 
-export function Settings({ job }) {
-    let required = getAllRequired(job.script)
-    let optional = getAllOptional(job.script)
-
+export function Settings({ job }: { job: Job }) {
+    const { pipeline } = useWindowStore()
+    const scriptDetails = pipeline.scripts.filter(
+        (s) => s.id == job.jobData.script.id
+    )[0]
+    if (!scriptDetails) {
+        return <p>Unrecognized script {job.jobData.script.id}</p>
+    }
     return (
         <ul>
-            {required.map((item, idx) => (
+            {getAllRequired(scriptDetails).map((item, idx) => (
                 <li key={idx}>
                     <span>{item.nicename}: </span>
                     <span>
@@ -15,7 +21,7 @@ export function Settings({ job }) {
                     </span>
                 </li>
             ))}
-            {optional.map((item, idx) => (
+            {getAllOptional(scriptDetails).map((item, idx) => (
                 <li key={idx}>
                     <span>{item.nicename}: </span>
                     <span>

3ea52d3edb6348d7f6ee05314462d39080121ba5
Marisa DeMeglio <marisa.demeglio@gmail.com>
2023-02-10 10:46:00 -0800

    fix: better visual consistency for new custom fields
    also adjusted some spacing and line heights on the new job view

--- src/renderer/components/Fields/CustomFieldDocumentation.tsx
+++ src/renderer/components/Fields/CustomFieldDocumentation.tsx
@@ -18,72 +18,49 @@ export function CustomFieldDocumentation({ datatypes }) {
         return { summary, details }
     })
 
-    if (datatypes.length == 1) {
-        return (
-            <details>
-                <summary>{allDocumentation[0].summary}</summary>
-                <div>
-                    <Markdown
-                        remarkPlugins={[remarkGfm]}
-                        components={{
-                            // override the rendering of link elements with a link element that opens in an external browser
-                            a: (props) => {
-                                return (
-                                    <a
-                                        href={props.href}
-                                        onClick={(e) =>
-                                            externalLinkClick(e, App)
-                                        }
-                                    >
-                                        {props.children}
-                                    </a>
-                                )
-                            },
-                        }}
-                    >
-                        {allDocumentation[0].details}
-                    </Markdown>
-                </div>
-            </details>
-        )
-    } else {
-        return (
-            <details className="documentation">
-                <summary>Allowed values</summary>
-                <ul>
-                    {allDocumentation.map((d) => {
-                        return (
-                            <>
-                                <li>{d.summary}
-                                    <Markdown
-                                        remarkPlugins={[remarkGfm]}
-                                        components={{
-                                            // override the rendering of link elements with a link element that opens in an external browser
-                                            a: (props) => {
-                                                return (
-                                                    <a
-                                                        href={props.href}
-                                                        onClick={(e) =>
-                                                            externalLinkClick(
-                                                                e,
-                                                                App
-                                                            )
-                                                        }
-                                                    >
-                                                        {props.children}
-                                                    </a>
-                                                )
-                                            },
-                                        }}
-                                    >
-                                        {d.details}
-                                    </Markdown>
-                                </li>
-                            </>
-                        )
-                    })}
-                </ul>
-            </details>
-        )
-    }
+    return (
+        <details className="documentation">
+            <summary>Allowed values</summary>
+            <ul>
+                {allDocumentation.map((d) => {
+                    return (
+                        <>
+                            <li>
+                                {d.summary}
+                                <div className="details">
+                                    {d.details ? (
+                                        <Markdown
+                                            remarkPlugins={[remarkGfm]}
+                                            components={{
+                                                // override the rendering of link elements with a link element that opens in an external browser
+                                                a: (props) => {
+                                                    return (
+                                                        <a
+                                                            href={props.href}
+                                                            onClick={(e) =>
+                                                                externalLinkClick(
+                                                                    e,
+                                                                    App
+                                                                )
+                                                            }
+                                                        >
+                                                            {props.children}
+                                                        </a>
+                                                    )
+                                                },
+                                            }}
+                                        >
+                                            {d.details}
+                                        </Markdown>
+                                    ) : (
+                                        ''
+                                    )}
+                                </div>
+                            </li>
+                        </>
+                    )
+                })}
+            </ul>
+        </details>
+    )
 }

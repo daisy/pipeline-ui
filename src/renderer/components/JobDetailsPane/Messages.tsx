61b03d1f1308aafe422333f1e8e2ba620ce2a5fe
Marisa DeMeglio <marisa.demeglio@gmail.com>
2024-06-28 15:36:23 -0700

    fix(#189): another idea for messages display
    This one has toggles for verbosity and warnings.
    
    Verbosity = ON means show everything
    Warnings = ON means show warnings even when verbosity is OFF

--- src/renderer/components/JobDetailsPane/Messages.tsx
+++ src/renderer/components/JobDetailsPane/Messages.tsx
@@ -1,31 +1,108 @@
+import { useState, useEffect } from 'react'
 import { Job, Message, MessageLevel, JobStatus } from 'shared/types'
 const { App } = window
 
 let messageSort = (a, b) => (a.sequence < b.sequence ? b : a)
 
-function MessageDisplay(m: Message, key) {
-    return (
-        <li key={key} className={MessageLevel[m.level].toLowerCase()}>
-            {m.level == 'INFO' ? m.content : `${m.level} - ${m.content}`}
-            {m.messages && m.messages.length > 0 && (
-                <ul>
-                    {m.messages.map((m, idx) =>
-                        MessageDisplay(m, `${key}-${idx}`)
-                    )}
-                </ul>
+function MessageDisplay(m: Message, key, depth, verbose, showWarnings) {
+    console.log('verbose', verbose)
+    // show messages that pass the verbosity filter
+    // always show error and warning
+    let renderMessageChildren = (messages) => (
+        <>
+            {messages.map((msg, idx) =>
+                MessageDisplay(
+                    msg,
+                    `${key}-${idx}`,
+                    depth + 1,
+                    verbose,
+                    showWarnings
+                )
             )}
-        </li>
+        </>
+    )
+
+    return (
+        <>
+            {verbose ||
+            (!verbose && depth < 2) ||
+            m.level == 'ERROR' ||
+            (showWarnings && m.level == 'WARNING') ? (
+                <li key={key} className={MessageLevel[m.level].toLowerCase()}>
+                    {m.level == 'INFO'
+                        ? m.content
+                        : `${m.level} - ${m.content}`}
+                    {verbose && m.messages && m.messages.length ? (
+                        <ul>{renderMessageChildren(m.messages)}</ul>
+                    ) : (
+                        ''
+                    )}
+                </li>
+            ) : (
+                ''
+            )}
+            {!verbose && m.messages && m.messages.length
+                ? renderMessageChildren(m.messages)
+                : ''}
+        </>
     )
 }
 
 export function Messages({ job }: { job: Job }) {
+    const [verbose, setVerbose] = useState(false)
+    const [showWarnings, setShowWarnings] = useState(true)
+
+    useEffect(() => {})
     return (
-        <ul>
-            {job.jobData.messages
-                ?.sort(messageSort)
-                .map((message: Message, idx) =>
-                    MessageDisplay(message, `log-${job?.internalId}-${idx}`)
-                )}
-        </ul>
+        <>
+            <div className="messageFilters">
+                <div>
+                    <label htmlFor={`${job.internalId}-showwarnings`}>
+                        Show warnings
+                    </label>
+                    {verbose ? (
+                        <input
+                            id={`${job.internalId}-showwarnings`}
+                            disabled
+                            type="checkbox"
+                            checked
+                        ></input>
+                    ) : (
+                        <input
+                            id={`${job.internalId}-showwarnings`}
+                            type="checkbox"
+                            //@ts-ignore
+                            onClick={(e) => setShowWarnings(e.target.checked)}
+                            defaultChecked={verbose || showWarnings}
+                        ></input>
+                    )}
+                </div>
+                <div>
+                    <label htmlFor={`${job.internalId}-verbose`}>Verbose</label>
+                    <input
+                        id={`${job.internalId}-verbose`}
+                        type="checkbox"
+                        onClick={(e) => {
+                            //@ts-ignore
+                            setVerbose(e.target.checked)
+                        }}
+                        defaultChecked={verbose}
+                    ></input>
+                </div>
+            </div>
+            <ul>
+                {job.jobData.messages
+                    ?.sort(messageSort)
+                    .map((message: Message, idx) =>
+                        MessageDisplay(
+                            message,
+                            `log-${job?.internalId}-${idx}`,
+                            0,
+                            verbose,
+                            showWarnings
+                        )
+                    )}
+            </ul>
+        </>
     )
 }

971a58f7400c348314889150aa74b34a5aa3ea94
Marisa DeMeglio <marisa.demeglio@gmail.com>
2024-10-11 12:59:42 -0700

    fix(#246): construct url dynamically
    Seems to not throw lifecycle errors anymore

--- src/renderer/utils/temp-datatype.ts
+++ src/renderer/utils/temp-datatype.ts
@@ -1,16 +1,13 @@
 import { datatypeXmlToJson } from 'shared/parser/pipelineXmlConverter'
 
-const { App } = window
-// import { baseurl } from 'shared/types'
-// import { useWindowStore } from 'renderer/store'
+import { baseurl } from 'shared/types'
+import { useWindowStore } from 'renderer/store'
 
 async function fetchTemporaryDatatype(id) {
     try {
-        // let href = `${baseurl(
-        //     useWindowStore().pipeline.webservice
-        // )}/datatypes/${id}`
-        // TODO construct URL dynamically; the above was causing react to throw lifecycle error
-        let href = `http://localhost:49152/ws/datatypes/${id}`
+        let href = `${baseurl(
+            useWindowStore().pipeline.webservice
+        )}/datatypes/${id}`
         // can't use IPC fetch due to async problems matching event send and receive
         // when multiple controls could be sending the same type of event
         // however this browser-based fetch is at risk of CORS issues depending on how the pipeline engine
@@ -19,7 +16,6 @@ async function fetchTemporaryDatatype(id) {
         // datatypes aren't at the global /datatypes endpoint
         let data = await fetch(href)
         let txt = await data.text()
-        console.log(`data for ${id}: \n`, txt)
         let datatypeAsJson = datatypeXmlToJson(href, id, txt)
         return datatypeAsJson ?? null
     } catch (err) {

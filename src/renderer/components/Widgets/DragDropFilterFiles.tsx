4f3940b517352aedbc2331bdb3fa81bf11ba04fb
Marisa DeMeglio <marisa.demeglio@gmail.com>
2025-12-18 12:19:05 -0800

    fix(#328): text filter for initial file list

--- src/renderer/components/Widgets/DragDropFilterFiles.tsx
+++ src/renderer/components/Widgets/DragDropFilterFiles.tsx
@@ -1,9 +1,10 @@
 import { FilelistWithRelevantScripts } from './FilelistWithRelevantScripts'
 import { getRelevantScripts } from '../scriptFilters'
 import { DragFileInput } from '../Widgets/DragFileInput'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FileTreeEntry } from 'main/ipcs/fileSystem'
 import { ID } from 'renderer/utils'
+import { X } from './SvgIcons'
 const { App } = window
 
 export function DragDropFilterFiles({
@@ -14,15 +15,47 @@ export function DragDropFilterFiles({
 }) {
     // files is [{filepath, filetype},...]
     const [files, setFiles] = useState(initialValue)
-    const [firstInteraction, setFirstInteraction] = useState(false)
+    const [filteredFiles, setFilteredFiles] = useState(initialValue)
+    const [filterText, setFilterText] = useState('')
+    const [uniqueFiletypes, setUniqueFiletypes] = useState(
+        Array.from(new Set(initialValue.map((f) => f.filetype.type)))
+    )
 
-    let updateFiles = (files_) => {
-        let filesCopy = [...files_]
-        setFiles(filesCopy)
-        onChange(filesCopy)
+    // update dependent states when files changes
+    useEffect(() => {
+        let filteredFiles_ = applyFilterToFiles()
+        setFilteredFiles(filteredFiles_)
+        onChange(files)
+    }, [files])
+
+    useEffect(() => {
+        setUniqueFiletypes(
+            Array.from(new Set(filteredFiles.map((f) => f.filetype.type)))
+        )
+    }, [filteredFiles])
+
+    useEffect(() => {
+        let filteredFiles_ = applyFilterToFiles()
+        setFilteredFiles(filteredFiles_)
+    }, [filterText])
+
+    let applyFilterToFiles = () => {
+        let filteredFiles_ = files.filter(
+            (f) =>
+                f.filepath.toLowerCase().indexOf(filterText.toLowerCase()) !=
+                    -1 ||
+                (f.filetype.name &&
+                    f.filetype.name
+                        .toLowerCase()
+                        .indexOf(filterText.toLowerCase()) != -1) ||
+                f.filetype.type
+                    .toLowerCase()
+                    .indexOf(filterText.toLowerCase()) != -1
+        )
+        console.log("Filtered files", filteredFiles_)
+        return filteredFiles_
     }
 
-    let uniqueFiletypes = Array.from(new Set(files.map((f) => f.filetype.type)))
     // handle user adding more files and folders
     // resolve folder contents and add files to the current files list
     let onDragInputChange = async (filenames) => {
@@ -62,7 +95,7 @@ export function DragDropFilterFiles({
         filesCopy = filesCopy.concat(
             uniqueNewFilesThatAreSupported.filter((f) => f.filetype != null)
         )
-        updateFiles(filesCopy)
+        setFiles(filesCopy)
     }
     // recursively list directory contents
     const resolveItems = async (items) => {
@@ -90,6 +123,7 @@ export function DragDropFilterFiles({
         }
         return resolvedItems
     }
+
     return (
         <div className="drag-drop-filter-files">
             {files.length == 0 && (
@@ -107,18 +141,53 @@ export function DragDropFilterFiles({
             />
 
             {files.length > 0 && (
-                <><p className="info">
-                    The following files can be used in Pipeline jobs:
-                </p>
-                <label htmlFor={`filterFilesInput-${job.internalId}`}></label>
-                <input id={`filterFilesInput-${job.internalId}`}></input>
+                <>
+                    <label htmlFor={`filterFilesInput-${job.internalId}`}>
+                        The following files can be used in Pipeline jobs. Type
+                        to filter the files listed:
+                    </label>
+                    <div className="file-filter">
+                        <input
+                            id={`filterFilesInput-${job.internalId}`}
+                            onChange={(e) => setFilterText(e.target.value)}
+                            value={filterText}
+                            onKeyDown={(e) =>
+                                e.key == 'Escape' ? setFilterText('') : ''
+                            }
+                        ></input>
+                        <button
+                            type="button"
+                            className="remove-button invisible"
+                            onClick={() => setFilterText('')}
+                            title="Clear filter text"
+                        >
+                            <X width="20" height="20" />
+                        </button>
+                    </div>
                 </>
             )}
-            {files.length > 0 && (
+            {filterText != '' && filteredFiles.length == 0 && (
                 <>
                     <div className="files">
+                        <p>No files to display with filter applied. </p>
+                    </div>
+                </>
+            )}
+            {filteredFiles.length > 0 && (
+                <>
+                    <div className="files">
+                        <p>
+                            {filterText != '' ? 'Filter applied. ' : ''}Showing{' '}
+                            {filteredFiles.length}{' '}
+                            {filteredFiles.length > 1 ? 'files' : 'file'} in{' '}
+                            {uniqueFiletypes.length}{' '}
+                            {uniqueFiletypes.length > 1
+                                ? 'categories'
+                                : 'category'}
+                            .
+                        </p>
                         {uniqueFiletypes.map((filetype, idx) => {
-                            let filesOfType = files
+                            let filesOfType = filteredFiles
                                 .filter((f) => f.filetype.type == filetype)
                                 .sort((a, b) => (a.name < b.name ? -1 : 1))
                             return (
@@ -135,7 +204,7 @@ export function DragDropFilterFiles({
                             )
                         })}
                     </div>
-                    <button onClick={() => updateFiles([])} type="button">
+                    <button onClick={() => setFiles([])} type="button">
                         Clear files
                     </button>
                 </>

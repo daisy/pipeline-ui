1a95e5742cd3e6d5953b63ede7c7b65f293e7977
Marisa DeMeglio <marisa.demeglio@gmail.com>
2026-03-20 15:40:57 -0700

    fix(messages): messages are in the correct sequence

--- src/renderer/components/Views/MainView/JobDetailsPane/Messages.tsx
+++ src/renderer/components/Views/MainView/JobDetailsPane/Messages.tsx
@@ -1,44 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Job, Message, MessageLevel } from 'shared/types'
 
-let messageSort = (a, b) => (a.sequence < b.sequence ? b : a)
-
-function MessageDisplay(m: Message, key, depth, verbose) {
-    // show messages that pass the verbosity filter
-    // always show error and warning
-    let renderMessageChildren = (messages) => (
-        <>
-            {messages.map((msg, idx) =>
-                MessageDisplay(msg, `${key}-${idx}`, depth + 1, verbose)
-            )}
-        </>
-    )
-
-    return (
-        <>
-            {verbose || m.level == 'ERROR' || m.level == 'WARNING' ? (
-                <li key={key} className={'msg-' + MessageLevel[m.level].toLowerCase()}>
-                    {m.level == 'INFO'
-                        ? m.content
-                        : `${m.level} - ${m.content}`}
-                    {verbose && m.messages && m.messages.length ? (
-                        <ul>{renderMessageChildren(m.messages)}</ul>
-                    ) : (
-                        ''
-                    )}
-                </li>
-            ) : (
-                ''
-            )}
-            {!verbose && m.messages && m.messages.length
-                ? renderMessageChildren(m.messages)
-                : ''}
-        </>
-    )
-}
+let messageSort = (a, b) => a.sequence - b.sequence
 
 export function Messages({ job }: { job: Job }) {
     const [verbose, setVerbose] = useState(false)
+    const [messages, setMessages] = useState(
+        structuredClone(job.jobData.messages).flat(Infinity).sort(messageSort)
+    )
+    useEffect(() => {
+        setMessages(
+            structuredClone(job.jobData.messages)
+                .flat(Infinity)
+                .sort(messageSort)
+        )
+    }, [job.jobData.messages])
 
     return (
         <>
@@ -55,16 +31,26 @@ export function Messages({ job }: { job: Job }) {
                 ></input>
             </div>
             <ul>
-                {job.jobData.messages
-                    ?.sort(messageSort)
-                    .map((message: Message, idx) =>
-                        MessageDisplay(
-                            message,
-                            `log-${job?.internalId}-${idx}`,
-                            0,
-                            verbose
-                        )
-                    )}
+                {messages
+                    .filter(
+                        (message) =>
+                            verbose ||
+                            message.level == 'ERROR' ||
+                            message.level == 'WARNING'
+                    )
+                    .map((message: Message, idx) => (
+                        <li
+                            key={`log-${job?.internalId}-${idx}`}
+                            className={
+                                'msg-' +
+                                MessageLevel[message.level].toLowerCase()
+                            }
+                        >
+                            {message.level == 'INFO'
+                                ? message.content
+                                : `${message.level} - ${message.content}`}
+                        </li>
+                    ))}
             </ul>
         </>
     )

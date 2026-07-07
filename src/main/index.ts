c2848b6a6b2595fc0763bfcdba39dcbfa0db5bcb
Marisa DeMeglio <marisa.demeglio@gmail.com>
2026-07-07 16:56:21 -0700

    fix(#400): exception debugging

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,7 +7,7 @@ import {
     nativeTheme,
 } from 'electron'
 
-import { error } from 'electron-log'
+import { error, info, warn } from 'electron-log'
 import * as fs from 'fs-extra'
 
 import {
@@ -62,6 +62,35 @@ import { sniffFile } from './ipcs/sniffFile'
 import { IPC_EVENT_openExternalFile } from 'shared/main-renderer-events'
 
 let pendingOpenFile: string | null = null
+function registerProcessDiagnostics() {
+    process.on('uncaughtException', (err) => {
+        error('main process uncaughtException', err)
+    })
+    process.on('unhandledRejection', (reason) => {
+        error('main process unhandledRejection', reason)
+    })
+    process.on('warning', (warning) => {
+        warn('main process warning', warning)
+    })
+    app.on('render-process-gone', (_event, webContents, details) => {
+        error('render process gone', {
+            reason: details.reason,
+            exitCode: details.exitCode,
+            url: webContents.getURL(),
+        })
+    })
+    app.on('child-process-gone', (_event, details) => {
+        error('child process gone', details)
+    })
+    app.on('before-quit', () => {
+        info('app before-quit')
+    })
+    app.on('will-quit', () => {
+        info('app will-quit')
+    })
+}
+
+registerProcessDiagnostics()
 
 app.on('open-file', (event, filePath) => {
     event.preventDefault()

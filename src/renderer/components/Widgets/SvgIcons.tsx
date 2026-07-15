a0e49fc8f8502facd782c369508ac2b68b69846f
Marisa DeMeglio <marisa.demeglio@gmail.com>
2026-07-15 16:49:54 -0700

    chore: replace library with single icon

--- src/renderer/components/Widgets/SvgIcons.tsx
+++ src/renderer/components/Widgets/SvgIcons.tsx
@@ -245,3 +245,22 @@ export function PauseIcon({ width, height }) {
         </svg>
     )
 }
+export function Upload({ width, height }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            viewBox="0 0 24 24"
+            role="presentation"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M12 3v12" />
+            <path d="m17 8-5-5-5 5" />
+            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+        </svg>
+    )
+}
